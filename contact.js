d398e4cc9a6e52cca2fa712713b1a69a103a7ef9
Adhritdas2009 <89207264+Adhritdas2009@users.noreply.github.com>
2022-03-01 22:02:44 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,15 @@
+function home(){
+    window.location="index.html"
+}
+
+function info(){
+    window.location="Flowers.html"
+}
+
+function contact(){
+    window.location="contact.html"
+}
+
+function talk(){
+    window.location="talk.html"
+}
